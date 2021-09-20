e2fb675f3d5407907e38b48a43212cc87a930358
Walidib <86203802+Walidib@users.noreply.github.com>
2021-09-20 02:45:37 +0100

    test

---
+++ request/DeviceStatusCreateRequest.java
@@ -0,0 +1,134 @@
+package com.sitewhere.rest.model.device.request;
+
+import com.sitewhere.rest.model.common.MetadataProvider;
+import com.sitewhere.spi.device.request.IDeviceStatusCreateRequest;
+
+/**
+ * Model object implementing {@link IDeviceStatusCreateRequest}.
+ * 
+ * @author Derek
+ */
+public class DeviceStatusCreateRequest extends MetadataProvider implements IDeviceStatusCreateRequest {
+
+    /** Serial version UID */
+    private static final long serialVersionUID = -1667891345754538713L;
+
+    /** Status code */
+    private String code;
+
+    /** Specification token */
+    private String specificationToken;
+
+    /** Display name */
+    private String name;
+
+    /** Background color */
+    private String backgroundColor;
+
+    /** Foreground color */
+    private String foregroundColor;
+
+    /** Border color */
+    private String borderColor;
+
+    /** Icon */
+    private String icon;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.sitewhere.spi.device.request.IDeviceStatusCreateRequest#getCode()
+     */
+    public String getCode() {
+	return code;
+    }
+
+    public void setCode(String code) {
+	this.code = code;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sitewhere.spi.device.request.IDeviceStatusCreateRequest#
+     * getSpecificationToken()
+     */
+    public String getSpecificationToken() {
+	return specificationToken;
+    }
+
+    public void setSpecificationToken(String specificationToken) {
+	this.specificationToken = specificationToken;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.sitewhere.spi.device.request.IDeviceStatusCreateRequest#getName()
+     */
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sitewhere.spi.device.request.IDeviceStatusCreateRequest#
+     * getBackgroundColor()
+     */
+    public String getBackgroundColor() {
+	return backgroundColor;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+	this.backgroundColor = backgroundColor;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sitewhere.spi.device.request.IDeviceStatusCreateRequest#
+     * getForegroundColor()
+     */
+    public String getForegroundColor() {
+	return foregroundColor;
+    }
+
+    public void setForegroundColor(String foregroundColor) {
+	this.foregroundColor = foregroundColor;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.sitewhere.spi.device.request.IDeviceStatusCreateRequest#
+     * getBorderColor()
+     */
+    public String getBorderColor() {
+	return borderColor;
+    }
+
+    public void setBorderColor(String borderColor) {
+	this.borderColor = borderColor;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.sitewhere.spi.device.request.IDeviceStatusCreateRequest#getIcon()
+     */
+    public String getIcon() {
+	return icon;
+    }
+
+    public void setIcon(String icon) {
+	this.icon = icon;
+    }
+}
